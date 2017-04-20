2e04841d3eae72dd592203154cc4ecb36e330a9f
warnerp18 <warnerp18@gmail.com>
2017-04-20 10:44:29 -0700

    add in sass compile and clean up

--- dist/app.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-console.log("hello world");
-
-var testing = function testing() {
-  console.log('fat arrow function');
-};
--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,43 @@
-var gulp = require('gulp');
-var babel = require("gulp-babel");
-var browserify = require("gulp-browserify");
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const rimraf = require('rimraf');
+const autoprefixer = require('gulp-autoprefixer');
+const babel = require("gulp-babel");
+const browserify = require("gulp-browserify");
 
-gulp.task('default', function() {
-  return gulp.src(['src/**.js'])
+gulp.task('clean', (callback) => {
+  rimraf('dist', callback);
+})
+
+gulp.task('move-html', ['clean'], () => {
+  return gulp.src('src/**/*.html')
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('compile-sass', () => {
+  rimraf('dist/**/*.css', ()=> {});
+
+  return gulp.src('src/stylesheets/index.scss')
+    .pipe(sass())
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(gulp.dest('dist/assets'));
+});
+
+gulp.task('bundle-js', function() {
+  return gulp.src(['src/**/**.js'])
     .pipe(browserify())
     .pipe(babel())
-    .pipe(gulp.dest("dist/js"));
+    .pipe(gulp.dest("dist/"));
 });
+
+gulp.task('default', ['move-html', 'compile-sass', 'bundle-js']);
+
+gulp.task('watch', () => {
+  gulp.watch('src/**/*.html', ['move-html']);
+  gulp.watch('src/**/*.scss', ['compile-sass']);
+  gulp.watch('src/**/*.js', ['bundle-js']);
+});
+
